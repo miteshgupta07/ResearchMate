7f380af548786025183185c00ab5f1c3638e44e2
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2025-02-24 18:25:04 +0530

    Changed

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ rag_page=st.Page(page="views/rag.py",
                  icon=":material/chat:",
                  default=True)
 
+agent_page=st.Page(page="views/agent.py",
+                   title="Agent",
+                   icon=":material/smart_toy:")
+
+about_app=st.Page(page="views/about_chatbot.py",
+                  title="About Chatbot",
+                  icon=":material/info:")
+
 about_me=st.Page(page="views/about_developer.py",
                  title="About Developer",
                  icon=":material/person:")
 
-
-pg=st.navigation(pages=[rag_page,about_me])
+pg=st.navigation(pages=[rag_page,agent_page,about_app,about_me])
 
 pg.run()
